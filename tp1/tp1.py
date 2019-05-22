f5d3ac39453ac2c848f8dabb5cca03bef95f4429
Renato <renato.junior@dcc.ufmg.br>
2019-05-22 13:04:58 -0300

    Fix flat shader

--- tp1/tp1.py
+++ tp1/tp1.py
@@ -308,22 +308,33 @@ def flat_shading():
     #version 130
 
     uniform mat4 model_view_matrix;
-    uniform mat4 normal_matrix;
     uniform mat4 model_view_projection_matrix;   
-      
+
+    out vec3 Vertex;
+    
+    void main()
+    {
+        Vertex = vec3(model_view_matrix * gl_Vertex);
+        gl_Position = model_view_projection_matrix * gl_Vertex;
+    }
+
+    """
+
+    fragment_shader = """
+    #version 130
+
+    in vec3 Vertex;
+
     uniform vec3 aColor;
     uniform vec3 lPosition;
     uniform vec3 lColor;
     uniform vec3 lSpecular;
     uniform float mShininess;
 
-    out vec4 newColor;
     void main()
     {
-        vec3 Vertex = vec3(model_view_matrix * gl_Vertex);
-        vec3 Normal = vec3(normalize(normal_matrix * vec4(gl_Normal, 0.0)));
-        gl_Position = model_view_projection_matrix * gl_Vertex;
-
+        vec3 Normal = normalize(cross(dFdx(Vertex), dFdy(Vertex)));
+        
         vec3 L = normalize(lPosition - Vertex);
         vec3 E = normalize(-Vertex);
         vec3 R = normalize(-reflect(L, Normal));
@@ -332,19 +343,7 @@ def flat_shading():
         vec4 diffuse = vec4(max(dot(L, Normal), 0) * lColor, 0.0);
         vec4 specular = vec4(lSpecular * pow(max(dot(R, E), 0.0), 0.3 * mShininess), 0.0);
         
-        newColor =  ambient + diffuse + specular;
-    }
-
-    """
-
-    fragment_shader = """
-    #version 130
-
-    in vec4 newColor;
-
-    void main()
-    {
-        gl_FragColor = newColor;
+        gl_FragColor =  ambient + diffuse + specular;
     }
     """
     shader = OpenGL.GL.shaders.compileProgram(OpenGL.GL.shaders.compileShader(vertex_shader, GL_VERTEX_SHADER),
